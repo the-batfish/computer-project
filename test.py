43a01526213ad29e5c5f7675bbee1cd9cf2defe6
adikpb <67222969+adikpb@users.noreply.github.com>
2021-05-18 09:24:24 +0400

    Make connection function, fix broken cursor

--- test.py
+++ test.py
@@ -3,35 +3,35 @@ from mysql.connector import errorcode
 import datetime
 import time
 
-try:
-    cnx = mysql.connector.connect(
-    host="blsuvxgq3bvwh8qw4ah7-mysql.services.clever-cloud.com",
-    user="uf7gxtzihchkojup",
-    password="K1bhziQq9KnSPAVSnFdH",
-    database='blsuvxgq3bvwh8qw4ah7' 
-    )
+def make_connection():
+    try:
+        cnx = mysql.connector.connect(
+        host="blsuvxgq3bvwh8qw4ah7-mysql.services.clever-cloud.com",
+        user="uf7gxtzihchkojup",
+        password="K1bhziQq9KnSPAVSnFdH",
+        database='blsuvxgq3bvwh8qw4ah7'
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    return cnx, cnx.cursor()
 
-
-
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
-
-cursor = cnx.cursor()
 
 def something():
-	query = "INSERT INTO exchange_rate(next_reset) VALUES('%s') "
-	value = datetime.datetime(2021,5,18,8,54,30).strftime('%Y-%m-%d %H:%M:%S')
-	cursor.execute("update exchange_rate SET next_reset = %s",(value,))
-	cnx.commit()
-	cnx.close()
+    cnx, cursor = make_connection()
+    query = "INSERT INTO exchange_rate(next_reset) VALUES('%s') "
+    value = datetime.datetime(2021,5,18,8,54,30).strftime('%Y-%m-%d %H:%M:%S')
+    cursor.execute("update exchange_rate SET next_reset = %s",(value,))
+    cnx.commit()
+    cnx.close()
 something()
 
 def recur():
+    cnx, cursor = make_connection()
     cursor.execute("select next_reset from exchange_rate")
     dt = datetime.datetime.strptime(cursor.fetchone()[0], '%Y-%m-%d %H:%M:%S')
     if datetime.datetime.now() >= dt:
