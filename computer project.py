2acd41f3bc5047ca4fb26e74bea3f8a8e28be8ea
the-batfish <74861519+the-batfish@users.noreply.github.com>
2021-08-23 22:29:34 +0400

    Update computer project.py

--- computer project.py
+++ computer project.py
@@ -175,9 +175,11 @@ def sell_crypto(num , username , currency): #here num is the number of cryptos b
 
 def exch_r8_refresh(currency):
     cnx, cursor = make_connection()
-    query1 = f"SELECT {currency} FROM {currency} ORDER BY date DESC LIMIT 1" 
+    query1 = f"SELECT {currency} , ratio FROM {currency} ORDER BY date DESC LIMIT 1" 
     cursor.execute(query1)
-    curr_exch_r8 = cursor.fetchone()[0]
+    results = cursor.fetchone()
+    curr_exch_r8 = results[0]
+    curr_ratio = results[1]
 
     query2 = f"SELECT {currency} FROM economy_data" 
     cursor.execute(query2)
@@ -199,15 +201,17 @@ def exch_r8_refresh(currency):
     if avg_money == 0:
         avg_money = 1
     
-    ratio = avg_crypto/avg_money
-    if round(curr_exch_r8*ratio) <= 1: 
+    ratio = round(avg_crypto/avg_money,2)
+    if ratio == curr_ratio:
+        new_exch_r8 = curr_exch_r8
+    elif round(curr_exch_r8*ratio) <= 1: 
         new_exch_r8 = 1
     elif round(curr_exch_r8*ratio) >= 100:
         new_exch_r8 = 100
     else:
         new_exch_r8 = round(curr_exch_r8*ratio)
     cnx.close()
-    return new_exch_r8
+    return new_exch_r8 , ratio
 
 def exch_r8_loop():
     while True:
@@ -217,9 +221,9 @@ def exch_r8_loop():
             results = cursor.fetchone()[0]
             dt = datetime.datetime.strptime(results, '%Y-%m-%d')
             if datetime.datetime.now() >= (dt + datetime.timedelta(days=1)):
-                curr_exch_r8 = exch_r8_refresh(i)
-                query = f"INSERT INTO {i}(date, {i}) VALUES(%s,%s)"
-                cursor.execute(query,(datetime.datetime.now().strftime('%Y-%m-%d'),curr_exch_r8))
+                curr_exch_r8 , ratio  = exch_r8_refresh(i)
+                query = f"INSERT INTO {i}(date, {i} , ratio) VALUES(%s,%s,%s)"
+                cursor.execute(query,(datetime.datetime.now().strftime('%Y-%m-%d'),curr_exch_r8,ratio))
                 cnx.commit()
                 sleep(5)
             cnx.close()
